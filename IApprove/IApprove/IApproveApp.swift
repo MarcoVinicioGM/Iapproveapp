6f570b1c16402c9f13a3cd574b29c331c449854b
myenne1 <145622683+myenne1@users.noreply.github.com>
2024-04-06 13:27:58 -0500

    Add files via upload

---
+++ IApprove/IApprove/IApproveApp.swift
@@ -0,0 +1,17 @@
+//
+//  IApproveApp.swift
+//  IApprove
+//
+//  Created by Muhanad Yennes on 4/6/24.
+//
+
+import SwiftUI
+
+@main
+struct IApproveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
