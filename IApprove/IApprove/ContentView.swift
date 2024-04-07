bf853ddc614ac13851a22bb7f33c9bce7634d30b
myenne1 <yennesmuhanad@gmail.com>
2024-04-07 00:07:20 -0500

    New addition to get barcode

--- IApprove/IApprove/ContentView.swift
+++ IApprove/IApprove/ContentView.swift
@@ -8,23 +8,31 @@
 import SwiftUI
 import VisionKit
     
-    struct ContentView: View {
-        @State private var capturedImage: UIImage? = nil
-        @State private var isCustomCameraViewPresented = false
-        
-        @State private var recognizedItems: [RecognizedItem] = []
+struct ContentView: View {
+    @State private var capturedImage: UIImage? = nil
+    @State private var isCustomCameraViewPresented = false
+    
+    @State private var recognizedItems: [RecognizedItem] = []
 
-        var body: some View {
-            NavigationView {
+    var body: some View {
+        NavigationView {
+            VStack {
                 DataScannerView(recognizedItems: $recognizedItems, recognizesMultipleItems: true)
                     .navigationBarTitle("Barcode Scanner")
+                
+                Spacer()
+                
+                List(recognizedItems, id:\.id) { item in
+                    if case let .barcode(barcode) = item {
+                        Text(barcode.payloadStringValue ?? "Barcode not found")
+                    }
+                }
             }
         }
-    }
-
-    struct ContentView_Previews: PreviewProvider{
-        static var previews: some View{
-            ContentView()
+        .onAppear {
+            print("Recognized Items: \(recognizedItems)")
         }
     }
+}
+
 
