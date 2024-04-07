3cadb8269dafdca82235e7e38484518d32f6acb6
MarcoVinicioGM <118138523+MarcoVinicioGM@users.noreply.github.com>
2024-04-06 22:30:14 -0500

    asdf

--- IApprove/IApprove/ContentView.swift
+++ IApprove/IApprove/ContentView.swift
@@ -6,41 +6,25 @@
 //
 
 import SwiftUI
-
-struct ContentView: View {
-    @State private var capturedImage: UIImage? = nil
-    @State private var isCustomCameraViewPresented = false
+import VisionKit
     
-    var body: some View{
-        ZStack{
-            if capturedImage != nil{
-                Image(uiImage: capturedImage!)
-                    .resizable()
-                    .scaledToFill()
-                    .ignoresSafeArea()
-            }else{
-                Color(UIColor.systemBackground)
-            }
-            VStack{
-                Spacer()
-                Button(action:{
-                    isCustomCameraViewPresented.toggle()
-                }, label: {
-                    Image(systemName: "camera.fill")
-                        .font(.largeTitle)
-                        .padding()
-                        .background(Color.black)
-                        .clipShape(Circle())
-                })
-                .padding(.bottom)
-                .sheet(isPresented: $isCustomCameraViewPresented, content: {CustomCameraView(capturedImage: $capturedImage)
-                })
+    struct ContentView: View {
+        @State private var capturedImage: UIImage? = nil
+        @State private var isCustomCameraViewPresented = false
+        
+        @State private var recognizedItems: [RecognizedItem] = []
+
+        var body: some View {
+            NavigationView {
+                DataScannerView(recognizedItems: $recognizedItems, recognizesMultipleItems: true)
+                    .navigationBarTitle("Barcode Scanner")
             }
         }
     }
+
     struct ContentView_Previews: PreviewProvider{
         static var previews: some View{
             ContentView()
         }
     }
-}
+
