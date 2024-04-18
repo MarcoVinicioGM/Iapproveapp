f418d135a7c5972c625d0f8c6e46356de7c50f30
myenne1 <yennesmuhanad@gmail.com>
2024-04-17 22:10:33 -0500

    Program now displays product name and ingredients on scan. Also deleted FoodAPI file.

--- IApprove/IApprove/ContentView.swift
+++ IApprove/IApprove/ContentView.swift
@@ -1,7 +1,6 @@
 import SwiftUI
 import VisionKit
 
-
 struct ContentView: View {
     @StateObject var viewModel = ProductViewModel()
     @State private var harmfulDict: [String] = [] // Array to store lines from the text file
@@ -13,113 +12,117 @@ struct ContentView: View {
         VStack {
             //VisionKit handles barcode scanning
             DataScannerView(recognizedItems: $recognizedItems, recognizesMultipleItems: true)
-                .navigationBarTitle("Scan your barcode")
-            
+                .navigationBarTitle("Scan Your Barcode")
+
             Spacer()
             //Creates list view that displays recognized Items only barcodes
-            List(recognizedItems, id: \.id) { item in
-                if case let .barcode(barcode) = item {
-                    Text(barcode.payloadStringValue ?? "Unknown barcode")
-                        .padding()
-                        .background(Color.gray.opacity(0.2))
-                        .cornerRadius(10)
+            VStack() {
+                List(recognizedItems, id: \.id) { item in
+                    if case let .barcode(barcode) = item {
+                        if !isTextFileRead {
+                            Text("Loading...")
+                                .onAppear() {
+                                    readTextFile()
+                                }
+                        } else {
+                            let productName = viewModel.label
+                            Text("Product: \(productName ?? "")")
+                                .onAppear {
+                                    viewModel.fetchProductInfo(barcode: barcode.payloadStringValue ?? "")
+                                }
+                            
+                            if let ingredients = viewModel.ingredients {
+                                let ingredientsArray = ingredients.lowercased().components(separatedBy: ", ")
+                                
+                                if !ingredientsArray.isEmpty {
+                                    Text("Ingredients: \(ingredientsArray.joined(separator: ", "))")
+                                } else {
+                                    Text("No harmful ingredients found")
+                                }
+                            } else {
+                                Text("Loading...")
+                            }
+                        }
+                    }
                 }
             }
-            .listStyle(PlainListStyle())
             .padding()
         }
-        
-        VStack {
-            if !isTextFileRead {
-                Text("Loading...")
-                    .onAppear {
-                        readTextFile()
-                    }
-            } else {
-                if let ingredients = viewModel.ingredients {
-                    let ingredientsArray = ingredients.lowercased().components(separatedBy: ", ")
-
-                    if !ingredientsArray.isEmpty {
-                        Text("Ingredients: \(ingredientsArray.joined(separator: ", "))")
-                    } else {
-                        Text("No harmful ingredients found")
-                    }
-                } else {
-                    Text("Loading...")
-                        .onAppear {
-                            
-                            let barcode = "0049000042566"
-                            viewModel.fetchProductInfo(barcode: barcode)
-                        }
-                }
-            }
-        }
-        .padding()
     }
 
     class ProductViewModel: ObservableObject {
-        @Published var ingredients: String?
+    @Published var ingredients: String?
+    @Published var label: String?
 
-        func fetchProductInfo(barcode: String) {
-            // Construct the URL for the Open Food Facts API endpoint
-            let baseURL = "https://world.openfoodfacts.org/api/v0/product/"
-            let urlString = baseURL + barcode + ".json"
-            guard let url = URL(string: urlString) else {
-                print("Invalid URL")
+    func fetchProductInfo(barcode: String) {
+        // Construct the URL for the Open Food Facts API endpoint
+        let baseURL = "https://world.openfoodfacts.org/api/v0/product/"
+        let urlString = baseURL + barcode + ".json"
+        guard let url = URL(string: urlString) else {
+            print("Invalid URL")
+            return
+        }
+
+        // Create a URLSession object
+        let session = URLSession.shared
+
+        // Create a data task for the URL
+        let task = session.dataTask(with: url) { (data, response, error) in
+            // Check for errors
+            if let error = error {
+                print("Error: \(error.localizedDescription)")
                 return
             }
 
-            // Create a URLSession object
-            let session = URLSession.shared
-
-            // Create a data task for the URL
-            let task = session.dataTask(with: url) { (data, response, error) in
-                // Check for errors
-                if let error = error {
-                    print("Error: \(error.localizedDescription)")
-                    return
-                }
-
-                // Check if a response was received
-                guard let httpResponse = response as? HTTPURLResponse,
-                      (200...299).contains(httpResponse.statusCode) else {
-                    print("Invalid response")
-                    return
-                }
-
-                // Check if data was returned
-                guard let responseData = data else {
-                    print("No data returned")
-                    return
-                }
-
-                do {
-                    // Parse the JSON response
-                    let json = try JSONSerialization.jsonObject(with: responseData, options: []) as! [String: Any]
-
-                    // Check if product exists in the response
-                    if let product = json["product"] as? [String: Any] {
-                        // Check if ingredients exist in the product
-                        if let ingredients = product["ingredients_text"] as? String {
-                            DispatchQueue.main.async {
-                                self.ingredients = ingredients
-                            }
-                        } else {
-                            print("Ingredients not found")
-                        }
-                    } else {
-                        print("Product information not found")
-                    }
-
-                } catch {
-                    print("Error parsing JSON: \(error.localizedDescription)")
-                }
+            // Check if a response was received
+            guard let httpResponse = response as? HTTPURLResponse,
+                  (200...299).contains(httpResponse.statusCode) else {
+                print("Invalid response")
+                return
             }
 
-            task.resume()
+            // Check if data was returned
+            guard let responseData = data else {
+                print("No data returned")
+                return
+            }
+
+            do {
+                // Parse the JSON response
+                let json = try JSONSerialization.jsonObject(with: responseData, options: []) as! [String: Any]
+
+                // Check if product exists in the response
+                if let product = json["product"] as? [String: Any] {
+                    // Check if ingredients exist in the product
+                    if let ingredients = product["ingredients_text"] as? String {
+                        DispatchQueue.main.async {
+                            self.ingredients = ingredients
+                        }
+                    } else {
+                        print("Ingredients not found")
+                    }
+                    
+                    // Check if label exists in the product
+                    if let productName = product["product_name"] as? String {
+                        DispatchQueue.main.async {
+                            self.label = productName
+                        }
+                    } else {
+                        print("Product name not found")
+                    }
+                } else {
+                    print("Product information not found")
+                }
+
+            } catch {
+                print("Error parsing JSON: \(error.localizedDescription)")
+            }
         }
+
+        task.resume()
     }
-    
+}
+
     func readTextFile() {
         // Read from txt file and add to list
         if let filePath = Bundle.main.path(forResource: "Harmful_Products", ofType: "txt") {
